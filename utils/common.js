18a4753d27d51cd8072e5e46b459c8abc29fd727
litsazib <roktimsazib92@gmail.com>
2023-01-11 10:46:05 +0600

    invilid file extension check

--- utils/common.js
+++ utils/common.js
@@ -6,3 +6,15 @@ export const isEmpty = (value)=>{
         return false
     }
 }
+
+export const isPdfFile = (fileUrl) => {
+    var ext = fileUrl?.substring(fileUrl.lastIndexOf('.') + 1);
+    if(ext == "pdf")
+    {
+        return true;
+    } 
+    else{
+        return false;
+    }
+}
+
