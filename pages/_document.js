056e76e749ec56d7135a0aea9d700a8712264419
litsazib <roktimsazib92@gmail.com>
2023-02-06 16:37:21 +0600

    modify

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html className="scroll-smooth">
       <Head>
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/icon.png"></link>
+      <meta name="theme-color" content="#fff" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap"
           rel="stylesheet"
