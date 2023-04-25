bdfb249c8d8891590417260ca548542808b353dc
litsazib <roktimsazib92@gmail.com>
2023-04-25 08:54:39 +0600

    modify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import ScrollToTop from "react-scroll-to-top";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div id="root">
       <Component {...pageProps} />
       <ScrollToTop smooth />
+      <MessengerCustomerChat
+        pageId="841264149232659"
+        appId="678102670990450"
+      />
     </div>
   );
 }
